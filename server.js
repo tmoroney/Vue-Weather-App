a2831e2bb41133a3813602f4d265f1f3bf95f75f
Tom Moroney <moroneyt@tcd.ie>
2023-10-14 13:26:47 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,210 @@
+import express from "express";
+import axios from "axios";
+import url from "url";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+const apiKey = 'c19a0bb7a62a97244341bb2b302a2859';
+const pexelsAPI = 'z19NEqU0Pj1txoPKDzUjVujpMVEEQ5oIOOsNfNMVdWo5sEwpkbTHFoGw';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+// Serve the "index.html" file
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/weather/:city", async (request, response) => {
+    const city = request.params.city;
+    try {
+        const locations = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=4&appid=${apiKey}`);
+        if (locations.data.length == 0) {
+            response.send({ error: 'No results found' });
+            return;
+        }
+        const lat = locations.data[0].lat;
+        const lon = locations.data[0].lon;
+        const result = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+        const airPolution = await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+
+        var weatherData = {
+            city: locations.data[0].name,
+            country: locations.data[0].country,
+            countryCode: locations.data[0].country.toLowerCase(),
+            lat: lat,
+            lon: lon,
+            type: '',
+            averageTemp: 0,
+            tempColor: 'Blue',
+            averageRainfall: 0,
+            averageWindSpeed: 0,
+            airQuality: 0,
+            airPolutionWarning: false,
+            highestAirPolution: 0,
+            daysWithRain: [],
+            umbrellaWarning: false,
+            imageUrl: '',
+            summary: {},
+            days: null
+        };
+
+        var totalPrecipitation = 0;
+        var totalWindSpeed = 0;
+        var totalTemp = 0;
+        var count = 1;
+        var info = {};
+        for (let i = 0; i < result.data.list.length; i++) {
+            var data = {
+                temp: result.data.list[i].main.temp,
+                feels_like: result.data.list[i].main.feels_like,
+                humidity: result.data.list[i].main.humidity,
+                precipitation: result.data.list[i].rain ? result.data.list[i].rain["3h"] : 0,
+                wind_speed: result.data.list[i].wind.speed,
+                description: result.data.list[i].weather[0].description,
+                time: result.data.list[i].dt_txt.split(' ')[1].replace(/:00$/, '')
+            };
+
+            var dayNum = new Date(result.data.list[i].dt_txt).getDay();
+            var day = dayNames[dayNum];
+
+            if (data.description.includes('rain') && !weatherData.daysWithRain.includes(day)) {
+                weatherData.daysWithRain.push(day);
+            }
+
+            count++;
+            totalTemp += data.temp;
+            totalPrecipitation += data.precipitation;
+            totalWindSpeed += data.wind_speed;
+            info[day] = info[day] ? info[day].concat(data) : [data];
+        }
+
+        weatherData.days = info;
+
+        var averageTemp = totalTemp / count;
+        if (averageTemp < 13) {
+            weatherData.type = 'cold';
+            weatherData.tempColor = 'blue';
+        } else if (averageTemp <= 23) {
+            weatherData.type = 'mild';
+            weatherData.tempColor = 'orange';
+        } else {
+            weatherData.type = 'hot';
+            weatherData.tempColor = 'red';
+        }
+
+        var count = 0;
+        var totalAirPolution = 0;
+        var highestAirPolution = 0;
+        while (count < airPolution.data.list.length) {
+            var airQuality = airPolution.data.list[count].main.aqi;
+            totalAirPolution += airQuality;
+            //var date = new Date(airPolution.data.list[count].dt * 1000);
+            count++;
+            if (airQuality > 10) {
+                weatherData.airPolutionWarning = true;
+            }
+            if (airQuality > highestAirPolution) {
+                highestAirPolution = airQuality;
+            }
+        }
+
+        weatherData.averageTemp = Math.floor(averageTemp);
+        weatherData.averageRainfall = (totalPrecipitation / count).toFixed(2);
+        weatherData.averageWindSpeed = (totalWindSpeed / count).toFixed(2);
+        weatherData.airQuality = Math.floor(totalAirPolution / count);
+        weatherData.highestAirPolution = highestAirPolution;
+
+        if (weatherData.daysWithRain.length > 0) {
+            weatherData.umbrellaWarning = true;
+        }
+
+        const imageData = await axios.get('https://api.pexels.com/v1/search', {
+            params: { query: `${weatherData.city}`, per_page: 1 }, // You can adjust 'per_page' as needed
+            headers: {
+                'Authorization': pexelsAPI,
+            },
+        });
+        if (imageData.data.photos.length > 0) {
+            weatherData.imageUrl = imageData.data.photos[0].src.landscape; // Get the first image from the search results
+        }
+
+        // add summary for each day
+        for (const day in weatherData.days) {
+            var precipitation = 0;
+            var totalTemperature = 0;
+            for (let i = 0; i < weatherData.days[day].length; i++) {
+                precipitation += weatherData.days[day][i].precipitation;
+                totalTemperature += weatherData.days[day][i].temp;
+            }
+            var averageTemperature = totalTemperature / weatherData.days[day].length;
+            weatherData.summary[day] = {
+                rain: precipitation.toFixed(2),
+                temp: Math.floor(averageTemperature)
+            };
+        }
+
+        response.send(weatherData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+app.get("/location/:city", async (request, response) => {
+    console.log("Recieved GET request for /location");
+    const city = request.params.city;
+    try {
+        const locations = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=4&appid=${apiKey}`);
+        var results = []
+        for (let i = 0; i < locations.data.length; i++) {
+            results.push({
+                name: locations.data[i].name,
+                country: locations.data[i].country,
+                lat: locations.data[i].lat,
+                lon: locations.data[i].lon
+            });
+        }
+        response.send(results);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+app.get("/weather/:lat/:lon", async (request, response) => {
+    const lat = request.params.lat;
+    const lon = request.params.lon;
+    try {
+        const result = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+        var weatherData = {};
+
+        for (let i = 0; i < result.data.list.length; i++) {
+            var data = {
+                temp: result.data.list[i].main.temp,
+                feels_like: result.data.list[i].main.feels_like,
+                humidity: result.data.list[i].main.humidity,
+                wind_speed: result.data.list[i].wind.speed,
+                description: result.data.list[i].weather[0].description,
+                time: result.data.list[i].dt_txt.split(' ')[1].replace(/:00$/, '')
+            };
+            var dayNum = new Date(result.data.list[i].dt_txt).getDay();
+            var day = dayNames[dayNum];
+            weatherData[day] = weatherData[day] ? weatherData[day].concat(data) : [data];
+        }
+        response.send(weatherData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+    console.log("Recieved GET request for /status");
+    response.send(status);
+});
